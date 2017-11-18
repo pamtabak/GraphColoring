d5392dea837fc3172521839c18ae2ccd4b641140
Pamela Tabak <pamelatabak@poli.ufrj.br>
2017-11-18 19:30:38 -0200

    improved python script that fixes input file

--- fixFile.py
+++ fixFile.py
@@ -1,11 +1,14 @@
 import sys
 import os
+import re
 
 filename  = 'Input/fixedTest.txt'
 with open(filename, 'w') as outfile:
-	with open('Input/test2.txt') as infile:
+	with open('Input/david.txt') as infile:
 		for line in infile:
-			numbers = line.split(' ')
+			regex     = re.search('(\d+\s\d+)', line)
+			regexLine = regex.group(0)
+			numbers   = regexLine.split(' ')
 			numbers[0] = int(numbers[0]) - 1;
 			numbers[1] = int(numbers[1]) - 1;
 			outfile.write(str(numbers[0]) + " " + str(numbers[1]) + "\n")
